b20751733849c1e4312c8d9500ec68a39266dd14
TutorialSpiller <mdjonesonly310@gmail.com>
2020-02-28 10:02:59 -0800

    first commit

---
+++ src/components/Friend.js
@@ -0,0 +1,26 @@
+import React, { Component } from "react";
+
+export default class Friend extends Component {
+  render() {
+    return (
+      <div
+        className="friend-component"
+        style={{ background: this.props.color }}
+      >
+        <div className="friend-header">
+          {this.props.isFavorite && (
+            <span role="img" aria-label="Star">
+              ⭐️
+            </span>
+          )}
+          <p className="subtle-text">Friend Component</p>
+        </div>
+        <div className="friend-contact">
+          <h4>{this.props.name}</h4>
+          {this.props.location && <h4>{this.props.location}</h4>}
+        </div>
+        <div className="skill-list" />
+      </div>
+    );
+  }
+}
